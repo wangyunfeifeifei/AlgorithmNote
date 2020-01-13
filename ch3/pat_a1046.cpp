0a2a87340687b248a656e913e93dd704c70ab288
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2020-01-13 19:27:50 +0800

    完成pat A 1046

---
+++ ch3/pat_a1046.cpp
@@ -0,0 +1,30 @@
+#include<cstdio>
+
+int main() {
+    int N;
+    int dis[100005]; // 1-n distance
+    int M;
+    long long int sum=0;
+    scanf("%d", &N);
+    dis[1]=0;
+    for(int i=2;i<=N+1;i++) {
+        int t;
+        scanf("%d", &t);
+        sum+=dis[i];
+        dis[i]=dis[i-1]+t;
+    }
+    scanf("%d", &M);
+    int a,b;
+    for(int i=1;i<=M;i++) {
+        scanf("%d%d", &a, &b);
+        if(a>b) {
+            int t=a;
+            a=b;
+            b=t;
+        }
+        int d=dis[b]-dis[a];
+        d=d>dis[N+1]-d?dis[N+1]-d:d;
+        printf("%d\n", d);
+    }
+    return 0;
+}
