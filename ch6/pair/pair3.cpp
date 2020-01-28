c60d17fc5cef17ac70ea00e0195df9f4f9aacfa9
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2020-01-28 14:51:43 +0800

    stl模板学习--pair

---
+++ ch6/pair/pair3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<map>
+#include<string>
+using namespace std;
+
+int main() {
+    map<string, int> mp;
+    mp.insert(make_pair("heihei", 5));
+    mp.insert(pair<string, int>("haha", 10));
+    for(map<string, int>::iterator it = mp.begin(); it != mp.end(); it ++) {
+        cout << it -> first <<" " << it->second<<endl;
+    }
+    return 0;
+}
